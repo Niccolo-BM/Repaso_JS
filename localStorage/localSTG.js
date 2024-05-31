25bbbbd7e5029967c4fed827fbc58c95287ede6c
Niccolo-BM <nicolasbernal0821@gmail.com>
2024-05-31 18:01:56 -0500

    ok

--- localStorage/localSTG.js
+++ localStorage/localSTG.js
@@ -1,53 +1,81 @@
 // localStorage (Almacenamiento local en Navegador, sera permanente siempre y cuando no lo borre o desinstale el programa)
 
-const numero = 28
-const lista =["hola", true, 3]
-const objeto = {
-    "name": "Nicolas",
-    1:"hola",
-    "edad": 31,    
-    "clan": "bezoz"
-}
+// const numero = 28
+// const lista =["hola", true, 3]
+// const objeto = {
+//     "name": "Nicolas",
+//     1:"hola",
+//     "edad": 31,    
+//     "clan": "bezoz"
+// }
 
-/* Insertar*/
-/*Gurardad nueva info o editarla */
-/*localStorage.setItem("key"(llave) , "value"(valor))*/
+// /* Insertar*/
+// /*Gurardad nueva info o editarla */
+// /*localStorage.setItem("key"(llave) , "value"(valor))*/
 
-localStorage.setItem("name", "Nicolas")
-localStorage.setItem("lastName", "Bernal")
+// localStorage.setItem("name", "Nicolas")
+// localStorage.setItem("lastName", "Bernal")
 
-localStorage.setItem("numero", numero)
-localStorage.setItem("lista", lista)
-localStorage.setItem("objeto",JSON.stringify(objeto))//PARSEAR:transformar un tipo de dato a otro
+// localStorage.setItem("numero", numero)
+// localStorage.setItem("lista", lista)
+// localStorage.setItem("objeto",JSON.stringify(objeto))//PARSEAR:transformar un tipo de dato a otro
 
-// localStorage.setItem("lastName", "Morales")//sobre escribe
+// // localStorage.setItem("lastName", "Morales")//sobre escribe
 
-/*OBTENER*/
-/*localStorage.setItem("key")*/  
+// /*OBTENER*/
+// /*localStorage.setItem("key")*/  
 
-const nombre = localStorage.getItem("name")
-const apellido = localStorage.getItem("lastName")
+// const nombre = localStorage.getItem("name")
+// const apellido = localStorage.getItem("lastName")
 
-let objetoLs = localStorage.getItem("objeto")
-objetoLs = JSON.parse(objetoLs)
-console.log(objetoLs[1])
+// let objetoLs = localStorage.getItem("objeto")
+// objetoLs = JSON.parse(objetoLs)
+// console.log(objetoLs[1])
 
-console.log(JSON.parse(`
-    [12, 12]
-`))
+// console.log(JSON.parse(`
+//     [12, 12]
+// `))
 
-console.log(typeof objetoLs)
-// console.log(nombre)
-// console.log(apellido)
+// console.log(typeof objetoLs)
+// // console.log(nombre)
+// // console.log(apellido)
 
-/*ELIMINAR*/
+// /*ELIMINAR*/
 
-localStorage.removeItem("name")
-// localStorage.removeItem("naem")
-// localStorage.clear()//borra todo lo almacenado
+// localStorage.removeItem("name")
+// // localStorage.removeItem("naem")
+// // localStorage.clear()//borra todo lo almacenado
 
 
 
+
+/* EJERCICIO 1*/
+
+const form = document.querySelector(".formulario")
+const titleInput = document.getElementById("inputTitulo")
+const title = document.querySelector("#title")
+
+form.addEventListener("submit", () =>{
+    
+    const info = titleInput.value
+    localStorage.setItem("title", info)
+    title.style= "color: yellowgreen; font-size: 50px;";
+
+    // const cambio = localStorage.getItem("title");
+    // title.textContent = cambio;
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+        const titleLs = localStorage.getItem("title")
+        
+        if (titleLs){
+            title.textContent = titleLs
+            
+        }
+
+        
+})
+    
 
 
 
