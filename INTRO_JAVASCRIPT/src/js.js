735d3b2ff63a3781510045d3da4e087bd09cce49
NicolasBernalM <138633064+Niccolo-BM@users.noreply.github.com>
2025-06-25 23:06:33 -0500

    Add files via upload

---
+++ INTRO_JAVASCRIPT/src/js.js
@@ -0,0 +1,115 @@
+
+
+// console.log("Que se dicen, que hay pa hacer",+32,"no se que escrbir",5*5);
+
+// // let cantidad = 32;
+// // const valor = 30;
+// // var otrovalor = 34;
+
+// /*17/04/2024 */
+// /**TIPOS DE DATOS**/
+
+// // const text ="Buenas, soy niccolo"
+// // console.log(text);
+// // console.log(typeof text);
+
+// var sinDefinir;
+// console.log(sinDefinir);
+// console.log(typeof sinDefinir);
+
+
+// const numerros =[1,2,3,4];
+// console.log (numerros);
+// console.log(typeof numerros);
+
+// /* TIPO DE FUNCIONES */
+
+// function saludar() {
+//         return "Buenas";
+// }
+// saludar();
+
+// //  FUNCTION
+// function suma(a,b) {
+//     return a+b;
+// }
+// console.log (suma(8,5));
+
+// //  EXPRESION
+// const sumar = function suma(a,b) {
+//     return a+b;
+// }
+// const a=5;
+// const b=6;
+// console.log("la suma es = ",sumar(a,b))
+
+// const resultado = sumar(5,6);
+// console.log(resultado);
+
+
+// // OBJETO X CONSTRUCTOR  NO SE USA
+// // const saludar = new function("return´hola´ ")
+
+// // ANONIMA ANONYMUS
+// const adicionar = function (a,b) {
+//     return a+b;
+// }
+// const num1=7;
+// const num2=6;
+// console.log("la suma es = ",adicionar(num1,num2))
+
+
+// //ARROW FUNCTION
+// const restar = a => a-4;
+// console.log("la resta es = ",restar(12))
+
+
+// // CALLBACKS
+
+// const fb = () => console.log("Funcion B ejecutandose");
+
+// const fa = a => a();
+// fa(fb);
+
+// // AUTOEJECUTABLES
+
+// console.log(
+//     (function saludar(){
+//         return "Hola"
+//     })()
+// );
+
+// console.log (
+//     (() => "Hello")()
+// )
+
+// 18-abr//
+
+/** PROPIEDAD DE LOS STRINGS**/
+
+const text = "Hola people" 
+
+// 19-abr //
+
+/**OPERADORES AVANZADOS**/
+
+// const x = 10;
+// const y = 5;
+// const z = 5;
+
+// console.log(x==y && y==z);
+// console.log(x==y || y==z);
+
+// const v = null;
+// const user = null;
+
+// console.log(v? x:z)
+
+// console.log(v??z);
+
+// if (user == null){
+//     console.log(z);
+// }
+
+// console.log(x??z);
+// console.log(!v);|
---
+++ INTRO_JAVASCRIPT/src/script.js
@@ -0,0 +1,7 @@
+//alert('Wasaaaaa')
+//recipiente = 'medias';
+alert(recipiente)
+
+String = 'cadena texto'
+Number = 12
+Boolean = false
